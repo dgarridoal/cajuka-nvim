dad7ba244dfbedf450574e47973b56493baea7da
Diego Garrido <dgarridoal@gmail.com>
2023-11-19 16:14:19 -0300

    Config LazyVim with devaslife

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,51 @@
+return {
+  -- Notify when Shift + K
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
+  },
+
+  -- Animation scroll
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = true,
+      }
+    end,
+  },
+
+  -- Logo
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    opts = function(_, opts)
+      local logo = [[
+ ██████╗ █████╗      ██╗██╗   ██╗██╗  ██╗ █████╗ 
+██╔════╝██╔══██╗     ██║██║   ██║██║ ██╔╝██╔══██╗
+██║     ███████║     ██║██║   ██║█████╔╝ ███████║
+██║     ██╔══██║██   ██║██║   ██║██╔═██╗ ██╔══██║
+╚██████╗██║  ██║╚█████╔╝╚██████╔╝██║  ██╗██║  ██║
+ ╚═════╝╚═╝  ╚═╝ ╚════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+      ]]
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+    end,
+  },
+}
