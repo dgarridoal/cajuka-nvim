ba25ba692781a6d82fc95e3a231f3913c5a84e93
Diego Garrido <dgarridoal@gmail.com>
2023-11-21 00:22:34 -0300

    Update keymaps like devaslife

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,10 +3,21 @@
 -- Add any additional keymaps here
 
 local keymap = vim.keymap
--- local opts = {
---   noremap = true,
---   silent = true,
--- }
+
+local opts = {
+  noremap = true,
+  silent = true,
+}
+
+-- Increment/Decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwords
+keymap.set("n", "dw", "vb_d")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
 
 -- Select All
 keymap.set("n", "<C-a>", "gg<S-v>G")
