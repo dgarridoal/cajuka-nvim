3ae54b0d57a6ed048e71ac73c3b5a21fc0531b94
Diego Garrido <dgarridoal@gmail.com>
2023-11-25 14:26:44 -0300

    Update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,7 +14,7 @@ keymap.set("n", "+", "<C-a>")
 keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwords
-keymap.set("n", "dw", "vb_d")
+keymap.set("n", "dw", "bved")
 
 -- Jumplist
 keymap.set("n", "<C-m>", "<C-i>", opts)
