c82b6689dd289542198b2a453b1ee21de8d7488b
Diego Garrido <dgarridoal@gmail.com>
2023-11-25 20:08:57 -0300

    Implements telescope and editor settings

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,6 +5,17 @@ return {
   opts = function()
     return {
       transparent = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = true,
+          indentscope_color = "",
+        },
+      },
     }
   end,
 }
