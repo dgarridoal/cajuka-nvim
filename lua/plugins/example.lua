150f415fc532056057270fa5930f6a77b0b87c03
Diego Garrido Alvial <dgarridoal@gmail.com>
2024-09-03 10:18:43 -0400

    chore: update example file

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -236,47 +236,99 @@ return {
       return {}
     end,
   },
+
   -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
       "hrsh7th/cmp-emoji",
     },
-    ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
+      opts.completion.autocomplete = false
+      opts.mapping["<CR>"] = nil
+      opts.window = {
+        completion = {
+          border = {
+            { "󱐋", "WarningMsg" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+          scrollbar = false,
+          winblend = 0,
+        },
+        documentation = {
+          border = {
+            { "󰙎", "DiagnosticHint" },
+            { "─", "Comment" },
+            { "╮", "Comment" },
+            { "│", "Comment" },
+            { "╯", "Comment" },
+            { "─", "Comment" },
+            { "╰", "Comment" },
+            { "│", "Comment" },
+          },
+          scrollbar = false,
+          winblend = 0,
+        },
+      }
     end,
   },
+
+  ------@param opts cmp.ConfigSchema
+  ---opts = function(_, opts)
+  ---  -- Define los bordes como una tabla de strings
+  ---  local custom_border = { "󱐋", "─", "╮", "│", "╯", "─", "╰", "│" }
+  ---  local doc_border = { "󰙎", "─", "╮", "│", "╯", "─", "╰", "│" }
+  ---
+  ---  -- Configuración de las ventanas
+  ---  opts.window = {
+  ---    completion = {
+  ---      border = custom_border, -- Usa la tabla de strings
+  ---      scrollbar = false,
+  ---      winblend = 0,
+  ---    },
+  ---    documentation = {
+  ---      border = doc_border, -- Usa la tabla de strings
+  ---      scrollbar = false,
+  ---      winblend = 0,
+  ---    },
+  ---  }
+  ---
+  ---  local has_words_before = function()
+  ---    unpack = unpack or table.unpack
+  ---    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  ---    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  ---  end
+  ---
+  ---  local luasnip = require("luasnip")
+  ---  local cmp = require("cmp")
+  ---
+  ---  opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  ---    ["<Tab>"] = cmp.mapping(function(fallback)
+  ---      if cmp.visible() then
+  ---        cmp.select_next_item()
+  ---      elseif luasnip.expand_or_jumpable() then
+  ---        luasnip.expand_or_jump()
+  ---      elseif has_words_before() then
+  ---        cmp.complete()
+  ---      else
+  ---        fallback()
+  ---      end
+  ---    end, { "i", "s" }),
+  ---    ["<S-Tab>"] = cmp.mapping(function(fallback)
+  ---      if cmp.visible() then
+  ---        cmp.select_prev_item()
+  ---      elseif luasnip.jumpable(-1) then
+  ---        luasnip.jump(-1)
+  ---      else
+  ---        fallback()
+  ---      end
+  ---    end, { "i", "s" }),
+  ---  })
+  ---end,
 }
