80fefffe04fd884b02f9b851f7b444aa93158d2d
Diego Garrido Alvial <dgarridoal@gmail.com>
2024-12-30 13:56:52 -0300

    update things

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,10 +12,6 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
-      opts = {
-        colorscheme = "solarized-osaka",
-        -- colorscheme = "sonokai",
-      },
     },
     -- import any extras modules here
 
@@ -41,7 +37,7 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = {} },
+  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
   performance = {
     rtp = {
